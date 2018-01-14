4d7a7b61b5910c509facc9c5e9aea174b79c141e
Iris Li <github@iris.li>
2018-01-13 16:43:26 -0800

    Add website layout and navigation

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'overview', // overview, explorer
+    };
+  }
   render() {
+    let headerNavOverviewClass = this.state.page === 'overview' ? 'active' : '';
+    let headerNavExplorerClass = this.state.page === 'explorer' ? 'active' : '';
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header className="AppHeader">
+          <div className="AppHeader__top">
+            <a href="#" className="AppHeader__top__titleLink"><h1 className="AppHeader__titleLink"><span className="letter">F</span>lexible<br /><span className="letter">T</span>rust<br /><span className="letter">W</span>eb</h1></a>
+            <nav>
+              <a href="#" className={headerNavOverviewClass}  onClick={() => {this.setState({page: 'overview'})}}>Overview</a>
+              <a href="#" className={headerNavExplorerClass}  onClick={() => {this.setState({page: 'explorer'})}}>Explorer</a>
+            </nav>
+          </div>
+          <div className="AppHeader__bottom">
+            <div className="AppHeader__bottom__links">
+              <a className="TODO">Github</a>
+            </div>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="App-content">
+          Page: {this.state.page}
+        </div>
       </div>
     );
   }
