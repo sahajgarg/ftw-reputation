5c7957805571573202f6597e7e7a4fcb5bc4ec0d
Sahaj Garg <sahajgarg@gmail.com>
2018-01-13 22:33:04 -0800

    dangling links, reorg

--- client/src/pagerank.js
+++ client/src/pagerank.js
@@ -1,36 +1,4 @@
 const numeric = require('numeric')
-const Web3 = require('web3')
-//const contractAbi = require('./abi.json')
-
-var web3 = new Web3("http://localhost:8545");
-
-//Replace address
-//const WTFcontract = new web3.eth.Contract(contractAbi, "0xdd73bcaf6cd68884edf2be65afe0bc7241dc9fbb");
-
-const handle_error = (error) => {
-	if(error) {
-		console.log(error);
-		throw(error);
-	}
-}
-
-const retreive = (callback) => {
-	// Retreive adjacency list and rating list from contract
-	WTFcontract.methods.get_node_list().call(function(error, response) {
-		handle_error(erorr);
-		node_list = response['1'];
-		WTFcontract.methods.get_edge_list().call(function(error, response) {
-			handle_error(error);
-			truster_list = response['0'];
-			trustee_list = response['1'];
-			trust_rating_list = response['2'];
-			if(callback) {
-				return callback({'node_list': node_list, 'truster_list': truster_list, 
-					'trustee_list': trustee_list, 'trust_rating_list': trust_rating_list});
-			}
-		});
-	});
-}
 
 const zeros = (nrows, ncols) => {
 	var x = new Array(nrows);
@@ -41,7 +9,7 @@ const zeros = (nrows, ncols) => {
 }
 
 //Take the sum over columns and optionally normalize that sum
-const colsum_and_norm = (m, normalize=false) => {
+const colsum_and_norm = (m, normalize=false, replace_col=null) => {
 	x = [];
 	sums = [];
 	for(var col = 0; col < m[0].length; col++) {
@@ -49,9 +17,13 @@ const colsum_and_norm = (m, normalize=false) => {
 		sum = numeric.sum(x);
 		sums.push(sum);
 
-		if(normalize && sum != 0) {
+		if(normalize) {
 			for(var row = 0; row < m.length; row++) {
-				m[row][col] /= sum;
+				if(sum != 0) {
+					m[row][col] /= sum;
+				} else {
+					m[row][col] = replace_col[row];
+				}
 			}
 		}
 	}
@@ -59,12 +31,12 @@ const colsum_and_norm = (m, normalize=false) => {
 }
 
 //In a graph, each row corresponds to someone you are trusting, and each column corresponds to a truster
-const construct_full_graph = (node_list, truster_list, trustee_list, trust_rating_list) => {
+const construct_graph = (node_list, truster_list, trustee_list, trust_rating_list, rank_source) => {
 	matrix = zeros(node_list.length, node_list.length);
 	for(i = 0; i < truster_list.length; i++) {
 		matrix[trustee_list[i]][truster_list[i]] = trust_rating_list[i];
 	}
-	colsum_and_norm(matrix, true);
+	colsum_and_norm(matrix, true, rank_source);
 
 	return matrix;
 }
@@ -78,8 +50,12 @@ const compute_page_rank = (adj_matrix, rank_source, personalization) => {
 
 	eig_ans = numeric.eig(pagerank_matrix);
 	real_condition = numeric.leq(numeric.abs(numeric.sub(eig_ans['lambda']['x'],1)), 0.01);
-	imag_condition = numeric.leq(numeric.abs(eig_ans['lambda']['y']), 0.01);
-	index = numeric.and(real_condition, imag_condition).indexOf(true);
+	if(eig_ans['lambda']['y'] == undefined) {
+		index = real_condition.indexOf(true);
+	} else {
+		imag_condition = numeric.leq(numeric.abs(eig_ans['lambda']['y']), 0.01);
+		index = numeric.and(real_condition, imag_condition).indexOf(true);
+	}
 	principal = [];
 	numeric._getCol(eig_ans['E']['x'], index, principal);
 	principal = numeric.div(principal, numeric.sum(principal))
@@ -92,7 +68,6 @@ const calculate = (data, rank_source=null, pubkey_rank_source=null, personalizat
 	truster_list = data['truster_list'];
 	trustee_list = data['trustee_list'];
 	trust_rating_list = data['trust_rating_list'];
-	adj_matrix = construct_full_graph(node_list, truster_list, trustee_list, trust_rating_list);
 
 	if (pubkey_rank_source == null && rank_source == null) 
 	{
@@ -104,25 +79,13 @@ const calculate = (data, rank_source=null, pubkey_rank_source=null, personalizat
 		rank_source[node_list.indexOf(pubkey_rank_source)] = 1
 	}
 
+	adj_matrix = construct_graph(node_list, truster_list, trustee_list, trust_rating_list, rank_source);
+	console.log(adj_matrix)
 	trust_values = compute_page_rank(adj_matrix, rank_source, personalization);
 	console.log(trust_values);
-	//console.log(rank_source)
 	
 	//ratings = compute_overall_ratings(global_rating_list, trust_values)
 
 }
 
-
-// Figure out what Iris is requesting from me -- for now, just compute and return 
-// {node_list, node_trust_scores, ratings, truster_list, trustee_list}
-const handler = (rank_source=null, pubkey_rank_source=null, personalization=0.15) => {
-	retreive(function(data) {
-		calculate(data, rank_source, pubkey_rank_source, personalization);
-	});
-}
-
-const tester = (data, rank_source=null, pubkey_rank_source=null, personalization=0.15) => {
-	calculate(data, rank_source, pubkey_rank_source, personalization);
-}
-
-module.exports = { tester };
+module.exports = { calculate };
--- client/src/test_pagerank.js
+++ client/src/test_pagerank.js
@@ -1,12 +1,56 @@
-const { tester } = require('./pagerank.js');
+const Web3 = require('web3')
+//const contractAbi = require('./abi.json')
+
+var web3 = new Web3("http://localhost:8545");
+
+//Replace address
+//const WTFcontract = new web3.eth.Contract(contractAbi, "0xdd73bcaf6cd68884edf2be65afe0bc7241dc9fbb");
+const { calculate } = require('./pagerank.js');
+
+//NON PAGERANK RELATED THINGS
+const handle_error = (error) => {
+	if(error) {
+		console.log(error);
+		throw(error);
+	}
+}
+
+const retreive = (callback) => {
+	// Retreive adjacency list and rating list from contract
+	WTFcontract.methods.get_node_list().call(function(error, response) {
+		handle_error(erorr);
+		node_list = response['1'];
+		WTFcontract.methods.get_edge_list().call(function(error, response) {
+			handle_error(error);
+			truster_list = response['0'];
+			trustee_list = response['1'];
+			trust_rating_list = response['2'];
+			if(callback) {
+				return callback({'node_list': node_list, 'truster_list': truster_list, 
+					'trustee_list': trustee_list, 'trust_rating_list': trust_rating_list});
+			}
+		});
+	});
+}
+
+// Figure out what Iris is requesting from me -- for now, just compute and return 
+// {node_list, node_trust_scores, ratings, truster_list, trustee_list}
+const handler = (rank_source=null, pubkey_rank_source=null, personalization=0.15) => {
+	retreive(function(data) {
+		calculate(data, rank_source, pubkey_rank_source, personalization);
+	});
+}
+
+const tester = (data, rank_source=null, pubkey_rank_source=null, personalization=0.15) => {
+	calculate(data, rank_source, pubkey_rank_source, personalization);
+}
 
 node_list = ['0', '1', '2', '3', '4']
-truster_list = [0, 0, 1, 2, 3, 4]
-trustee_list = [1, 2, 0, 1, 4, 3]
+truster_list = [0, 0, 1, 3, 4]
+trustee_list = [1, 2, 0, 4, 3]
 trust_rating_list = [1, 1, 1, 1, 1, 1]
 
 data = {'node_list': node_list, 'truster_list': truster_list, 
 	'trustee_list': trustee_list, 'trust_rating_list': trust_rating_list};
 
 tester(data, undefined, '0')
-
